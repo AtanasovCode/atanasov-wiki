b7cbe68cb446605d4fab868fc7441d432319ee02
AtanasovCode <a_ljupce@yahoo.com>
2026-07-14 19:51:10 +0200

    install zustand & create new store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-const App = () => {
-  return (
-    <div className="min-h-screen w-full bg-graphene">
+import { useWikiStore } from "./useWikiStore";
 
+
+const App = () => {
+
+  const { bears } = useWikiStore()
+
+  return (
+    <div className="min-h-screen w-full bg-graphene text-text-dark">
+      <p>{bears}</p>
     </div>
   );
 }
---
+++ src/useWikiStore.tsx
@@ -0,0 +1,16 @@
+import { create } from "zustand"
+
+
+// Define types for state & actions
+interface WikiState {
+  bears: number
+  food: string
+  feed: (food: string) => void
+}
+
+// Create store using the curried form of `create`
+export const useWikiStore = create<WikiState>()((set) => ({
+  bears: 2,
+  food: 'honey',
+  feed: (food) => set(() => ({ food })),
+}))
