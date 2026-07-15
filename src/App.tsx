3a5606063ef032ad5ddaa61b6969dd2c27431243
AtanasovCode <a_ljupce@yahoo.com>
2026-07-15 19:35:19 +0200

    add theme changing based on store state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useWikiStore } from "./useWikiStore";
+import clsx from "clsx";
 
 
 const App = () => {
 
-  const { bears } = useWikiStore()
+  const { theme } = useWikiStore()
 
   return (
-    <div className="dark min-h-screen w-full bg-background-light text-text-light dark:bg-background-dark dark:text-text-dark">
+    <div
+      className={clsx(
+        "min-h-screen w-full bg-background-light text-text-light",
+        "dark:bg-background-dark dark:text-text-dark",
+        theme == "dark" && "dark"
+      )}
+    >
       <div className="font-bold">TEST</div>
     </div>
   );
