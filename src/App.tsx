7b3cdf8250fcae3817667d2f399f36a9b5839869
AtanasovCode <a_ljupce@yahoo.com>
2026-07-15 19:27:45 +0200

    install clsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   const { bears } = useWikiStore()
 
   return (
-    <div className="min-h-screen w-full bg-background-light text-text-light dark:bg-background-dark dark:text-text-dark">
+    <div className="dark min-h-screen w-full bg-background-light text-text-light dark:bg-background-dark dark:text-text-dark">
       <div className="font-bold">TEST</div>
     </div>
   );
