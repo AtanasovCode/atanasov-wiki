a752893b0267f2d846ed80eedbace9cfc13975e1
AtanasovCode <a_ljupce@yahoo.com>
2026-07-24 12:54:58 +0200

    create section height observer for current article

--- src/components/main/MainBody.tsx
+++ src/components/main/MainBody.tsx
@@ -1,4 +1,5 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
+import { useWikiStore } from "../../useWikiStore";
 import Heading from "../text-elements/Heading";
 import type { ArticleContentTypes } from "../../types/articleTypes";
 
@@ -12,7 +13,34 @@ const MainBody = ({
     currentArticle,
 }: MainBodyProps) => {
 
+    const { setSectionHeights } = useWikiStore()
     const sectionRefs = useRef<any>({})
+
+    useEffect(() => {
+        if(!currentArticle || currentArticle.length === 0) return
+
+        const updateHeights = () => {
+            const newHeights: Record<string, number> = {}
+
+            currentArticle.forEach((section) => {
+                const element = sectionRefs.current[section.sectionId]
+
+                if(element) {
+                    newHeights[section.sectionId] = element.getBoundingClientRect().height
+                }
+            })
+            setSectionHeights(newHeights)
+        }
+
+        const resizeObserver = new ResizeObserver(() => updateHeights())
+        currentArticle.forEach((section) => {
+            const element = sectionRefs.current[section.sectionId]
+            if(element) resizeObserver.observe(element)
+        })
+
+        updateHeights()
+        return () => resizeObserver.disconnect()
+    }, [currentArticle, setSectionHeights])
 
     return (
         <div
