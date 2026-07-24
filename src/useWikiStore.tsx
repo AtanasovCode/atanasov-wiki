c5f831ed2fc93f99f7ed901ff217d4a9cec7ad71
AtanasovCode <a_ljupce@yahoo.com>
2026-07-24 12:22:58 +0200

    create section height state in store

--- src/useWikiStore.tsx
+++ src/useWikiStore.tsx
@@ -14,18 +14,26 @@ interface WikiState {
 
   websiteHeight: number
   setWebsiteHeight: (height: number) => void
+
+  sectionHeights: Record<string, number>
+  setSectionHeights: (height: Record<string, number>) => void
 }
 
 
 
 // Create store using the curried form of `create`
 export const useWikiStore = create<WikiState>()((set) => ({
+  // ----------------------------------------------------- Website Customization
   theme: "dark",
   setTheme: (theme) => set(() => ({ theme })),
   font: "sans",
   setFont: (font) => set(() => ({ font })),
   fontSize: "normal",
   setFontSize: (fontSize) => set(() => ({ fontSize })),
+
+  // ----------------------------------------------------- Website Element Heights
   websiteHeight: 0,
   setWebsiteHeight: (websiteHeight) => set(() => ({ websiteHeight })),
+  sectionHeights: {},
+  setSectionHeights: (sectionHeights) => set(() => ({ sectionHeights })),
 }))
