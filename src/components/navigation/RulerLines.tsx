7d3b151e73c791fedca73b33e97f69a9a62b4870
AtanasovCode <a_ljupce@yahoo.com>
2026-07-24 16:16:49 +0200

    add minor ticks to ruler nav

--- src/components/navigation/RulerLines.tsx
+++ src/components/navigation/RulerLines.tsx
@@ -9,12 +9,31 @@ interface RulerLinesProps {
 
 
 
+const TICK_SPACING: number = 10
+const TICK_HEIGHT: number = 2
 
 const RulerLines = ({
     article,
 }: RulerLinesProps) => {
 
     const { sectionHeights } = useWikiStore()
+
+    const generateTicks = (height: number) => {
+        const ticks = []
+        const availableHeight: number = height - TICK_HEIGHT // Subtract major tick height
+        const tickCount: number = Math.floor(availableHeight / TICK_SPACING)
+
+        for (let i = 0; i < tickCount; i++) {
+            ticks.push(
+                <div
+                    className="w-2 bg-secondary"
+                    style={{ height: `${TICK_HEIGHT}px` }}
+                ></div>
+            );
+        }
+
+        return ticks;
+    }
 
     return (
         <div className="h-full w-full flex flex-col items-end justify-end">
@@ -28,11 +47,22 @@ const RulerLines = ({
                             className="flex items-start justify-start"
                             style={{ height: `${ratioHeight}px` }}
                         >
+                            {/* Major Tick */}
                             <div className="h-0.5 flex items-center jusitfy-start gap-2">
                                 <div className="whitespace-nowrap">
                                     {section.sectionTitle}
                                 </div>
-                                <div className="w-6 h-0.5 bg-secondary"></div>
+                                <div
+                                    className="w-6 bg-secondary"
+                                    style={{ height: `${TICK_HEIGHT}px` }}
+                                ></div>
+                            </div>
+
+                            {/* Minor Tick */}
+                            <div className="h-full flex flex-col items-end justify-between">
+                                {
+                                    generateTicks(ratioHeight)
+                                }
                             </div>
                         </div>
                     );
