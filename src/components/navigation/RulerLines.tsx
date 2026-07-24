49d53155e7b7747226c686543a4697b2fa5b03a9
AtanasovCode <a_ljupce@yahoo.com>
2026-07-24 16:46:10 +0200

    fix spacing issue with minor ticks

--- src/components/navigation/RulerLines.tsx
+++ src/components/navigation/RulerLines.tsx
@@ -9,7 +9,7 @@ interface RulerLinesProps {
 
 
 
-const TICK_SPACING: number = 10
+const TICK_SPACING: number = 12
 const TICK_HEIGHT: number = 2
 
 const RulerLines = ({
@@ -26,6 +26,7 @@ const RulerLines = ({
         for (let i = 0; i < tickCount; i++) {
             ticks.push(
                 <div
+                    key={i}
                     className="w-2 bg-secondary"
                     style={{ height: `${TICK_HEIGHT}px` }}
                 ></div>
@@ -38,17 +39,20 @@ const RulerLines = ({
     return (
         <div className="h-full w-full flex flex-col items-end justify-end">
             {
-                article.map((section) => {
+                article.map((section, idx) => {
                     const actualHeight = sectionHeights[section.sectionId]
                     const ratioHeight = actualHeight / HEIGHT_RATIO
                     return (
                         <div
                             key={section.sectionId}
-                            className="flex items-start justify-start"
+                            className="flex flex-col items-end justify-end"
                             style={{ height: `${ratioHeight}px` }}
                         >
                             {/* Major Tick */}
-                            <div className="h-0.5 flex items-center jusitfy-start gap-2">
+                            <div
+                                className="flex items-center jusitfy-start gap-2 my-1.5"
+                                style={{ height: `${TICK_HEIGHT}px`, marginTop: `${idx === 0 && 0}px` }}
+                            >
                                 <div className="whitespace-nowrap">
                                     {section.sectionTitle}
                                 </div>
@@ -59,7 +63,7 @@ const RulerLines = ({
                             </div>
 
                             {/* Minor Tick */}
-                            <div className="h-full flex flex-col items-end justify-between">
+                            <div className="flex-1 flex flex-col items-end justify-between">
                                 {
                                     generateTicks(ratioHeight)
                                 }
